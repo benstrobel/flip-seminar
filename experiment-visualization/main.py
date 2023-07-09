f19927ea18b8ebd2578a6367b942265b27bec6d4
Ben Strobel <benedikt.strobel@tum.de>
2023-07-09 23:56:32 +0200

    Created plots for experiment

--- experiment-visualization/main.py
+++ experiment-visualization/main.py
@@ -1,13 +1,28 @@
 import json
 import matplotlib.pyplot as plt
 
-with open('experiments/1/log.json') as f:
-    entries = json.load(f)
-    plt.plot([x["localErrorVal"] for x in entries], label="Local Validation Loss")
-    plt.plot([x["federatedErrorVal"] for x in entries], label="Federated Validation Loss")
-    plt.plot([x["localErrorTrain"] for x in entries], label="Local Train Loss")
-    plt.plot([x["federatedErrorTrain"] for x in entries], label="Federated Train Loss")
-    plt.legend()
-    plt.xlabel("Epochs")
-    plt.ylabel("Binary Cross Entropy Loss")
-    plt.show()
+with open('experiments/2/0.json') as f0:
+    with open('experiments/2/1.json') as f1:
+        entries0 = json.load(f0)
+        entries1 = json.load(f1)
+        plt.plot([x["localErrorVal"] for x in entries0[:40]], label="Local Train Loss 0", color="blue")
+        plt.plot([x["localErrorTrain"] for x in entries0[:40]], label="Local Validation Loss 0", color="purple")
+        plt.plot([x["localErrorVal"] for x in entries1[:40]], label="Local Validation Loss 1", color="orange")
+        plt.plot([x["localErrorTrain"] for x in entries1[:40]], label="Local Train Loss 1", color="red")
+        plt.legend()
+        plt.xlabel("Epochs")
+        plt.ylabel("Binary Cross Entropy Loss")
+        plt.show()
+
+with open('experiments/2/0.json') as f0:
+    with open('experiments/2/1.json') as f1:
+        entries0 = json.load(f0)
+        entries1 = json.load(f1)
+        plt.plot([x["federatedErrorVal"] for x in entries0[:40]], label="Federated Validation Loss 0", color="purple")
+        plt.plot([x["federatedErrorTrain"] for x in entries0[:40]], label="Federated Train Loss 0", color="blue")
+        plt.plot([x["federatedErrorVal"] for x in entries1[:40]], label="Federated Validation Loss 1", color="orange")
+        plt.plot([x["federatedErrorTrain"] for x in entries1[:40]], label="Federated Train Loss 1", color="red")
+        plt.legend()
+        plt.xlabel("Epochs")
+        plt.ylabel("Binary Cross Entropy Loss")
+        plt.show()
